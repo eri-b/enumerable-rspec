f120cd86deb10fe445c38ffb16e612854a016130
erib <eri-b@users.noreply.github.com>
2019-05-31 17:14:18 -0400

    removed hard codes

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,48 +2,78 @@ require_relative '../lib/enumerable'
 
 RSpec.describe Enumerable do
   describe "#my_each" do
+    let(:arr) {[1,2,3,4,5]}
     it "cycles through an array and returns itself" do
-      expect([0,4,5].my_each {|x| 2*x}).to eql([0,4,5])
+      output = arr.my_each {|x| 2*x}
+      result = arr.each {|x| 2*x}
+      expect(output).to eql(result)
     end
   end
 
   describe "#my_select" do
+    let(:arr) {[1,2,3,4,5]}
     it "selects portion of array based on condition" do
-      expect([0,4,5].my_select {|x| x>2}).to eql([4,5])
+      output = arr.my_select {|x| x > 2}
+      result = arr.select {|x| x > 2}
+      expect(output).to eql(result)
+      # expect([0,4,5].my_select {|x| x>2}).to eql([4,5])
     end
   end
 
   describe "#my_all?" do
+    let(:arr) {[1,2,3,4,5]}
     it "returns true if all values of array pass condition" do
-      expect([3,4,5].my_all? {|x| x>2}).to eql(true)
+      output = arr.my_all? {|x| x > 0}
+      result = arr.all? {|x| x > 0}
+      expect(output).to eql(result)
+      # expect([3,4,5].my_all? {|x| x>2}).to eql(true)
     end
     it "returns false if one value of array fails condition" do
-      expect([1,4,5].my_all? {|x| x>2}).to eql(false)
+      output = arr.my_all? {|x| x > 2}
+      result = arr.all? {|x| x > 2}
+      expect(output).to eql(result)
+      # expect([1,4,5].my_all? {|x| x>2}).to eql(false)
     end
   end
 
   describe "#my_count" do
+    let(:arr) {[1,2,3,4,5]}
     it "counts elements of array" do
-      expect([0,4,5].count).to eql(3)
+      output = arr.my_count
+      result = arr.count
+      expect(output).to eql(result)
+      # expect([0,4,5].count).to eql(3)
     end
     it "counts elements of array that satisfy condition" do
-      expect([0,4,5].count {|x| x > 4 }).to eql(1)
+      output = arr.my_count {|x| x > 2}
+      result = arr.count {|x| x > 2}
+      expect(output).to eql(result)
+      # expect([0,4,5].count {|x| x > 4 }).to eql(1)
     end
   end
 
   describe "#my_map" do
+    let(:arr) {[1,2,3,4,5]}
     it "returns updated array based on operation" do
-      expect([0,4,5].my_map{|x| x*3}).to eql([0,12,15])
+      output = arr.my_map {|x| x * 2}
+      result = arr.map {|x| x * 2}
+      expect(output).to eql(result)
+      # expect([0,4,5].my_map{|x| x*3}).to eql([0,12,15])
     end
   end
 
   describe "#my_inject" do
+    let(:arr) {[1,2,3,4,5]}
     it "returns sum of array" do
-      expect([9,4,5].my_inject{|prev, cur| prev+cur}).to eql(18)
+      output = arr.my_inject {|prev, cur| prev + cur}
+      result = arr.inject {|prev, cur| prev + cur}
+      # expect([9,4,5].my_inject{|prev, cur| prev+cur}).to eql(18)
     end
 
     it "returns product of array" do
-      expect([2,4,5].my_inject{|prev, cur| prev*cur}).to eql(40)
+      output = arr.my_inject {|prev, cur| prev * cur}
+      result = arr.inject {|prev, cur| prev * cur}
+      # expect([2,4,5].my_inject{|prev, cur| prev*cur}).to eql(40)
     end
   end
 
